12c3d36359e1170af284d8bdd035ae5bf439d666
RodrigoCasale <rodrigo_casale22@hotmail.com>
2019-05-08 15:28:24 -0500

    c

--- src/pages/FDI.js
+++ src/pages/FDI.js
@@ -7,8 +7,8 @@ const SecondPage = () => (
   <div className="cobo"><h1>AMIGA</h1><h2>Beta 1.0</h2>
 <iframe
     allow="microphone;"
-    width="350"
-    height="430"
+    width="500"
+    height="500"
     src="https://console.dialogflow.com/api-client/demo/embedded/aee7c6e1-16f0-4b0e-bba0-521fbaa7acc7">
 </iframe>
   </div>
