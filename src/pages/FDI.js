0603b5ddaa1fce7b3619f1b024c8f4ec99cc99c7
RodrigoCasale <rodrigo_casale22@hotmail.com>
2019-05-08 16:23:54 -0500

    c

--- src/pages/FDI.js
+++ src/pages/FDI.js
@@ -4,13 +4,14 @@ import Link from 'gatsby-link'
 
 import './FDI.css'
 const SecondPage = () => (
-  <div className="cobo"><h1>AMIGA</h1><h2>Beta 1.0</h2>
-<iframe
-    allow="microphone;"
-    width="500"
-    height="500"
-    src="https://console.dialogflow.com/api-client/demo/embedded/aee7c6e1-16f0-4b0e-bba0-521fbaa7acc7">
-</iframe>
+  <div className="cobo">
+    <div className="chi"><h1>AMIGA</h1><h2>Beta 1.0</h2></div>
+    <iframe
+        allow="microphone;"
+        width="500"
+        height="500"
+        src="https://console.dialogflow.com/api-client/demo/embedded/aee7c6e1-16f0-4b0e-bba0-521fbaa7acc7">
+    </iframe>
   </div>
 )
 
